0ae6bb58c56e91dc4bb805625bd41643bb30cec2
Anirudh Emmadi <kanna6501@gmail.com>
2018-03-28 14:08:51 -0500

    Updated Todo-list

--- Javascript/Todo-list/script.js
+++ Javascript/Todo-list/script.js
@@ -1,15 +1,30 @@
 var todos = [];
-
 var input = prompt("What would you like to do?");
 while(input !== "quit"){
-
   if(input == "list"){
-    console.log(todos);
+    listTodos();
+  }else if(input == "new"){
+    newTodos();
+  }else if(input == "delete"){
+    deleteTodos();
   }
-  else if(input == "new"){
-    var newTodo = prompt("Enter new To-Do");
-    todos.unshift(newTodo);
-  }
-  var input = prompt("What would you like to do?");
+  input = prompt("What would you like to do?");
 }
 console.log("You Quit the APP");
+function listTodos(){
+  todos.forEach(function(todo, i){
+    console.log("**************");
+    console.log(i + ": " + todo);
+    console.log("**************");
+  });
+}
+function newTodos(){
+  var newTodo = prompt("Enter new To-Do");
+  todos.push(newTodo);
+  console.log("Added Todo");
+}
+function deleteTodos(){
+  var index = prompt("Enter index of the To-Do to delete");
+  todos.splice(index, 1);
+  console.log("Deleted Todo");
+}
