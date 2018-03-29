ea533483ea344d60e8be6d63454fe651dba4a974
Anirudh Emmadi <kanna6501@gmail.com>
2018-03-29 12:52:42 -0500

    Added Object Excersise

---
+++ Javascript/Objects Excersise/script.js
@@ -0,0 +1,12 @@
+var someObject = {
+  friends: [
+    {name: "Malfoy"},
+    {name: "Crabbe"},
+    {name: "Goyle"}
+  ],
+  color: "baby blue",
+  isEvil: true
+};
+
+var malfoy = someObject.friends[0].name;
+console.log(malfoy);
