c2582e1f317caaa6daa9c1c5758e460ca57a93e2
Anirudh Emmadi <kanna6501@gmail.com>
2018-04-07 16:13:33 -0500

    clickedColor Not Working

--- Projects/Color Guessing Game/script.js
+++ Projects/Color Guessing Game/script.js
@@ -1,3 +1,4 @@
+//initialized colors array
 var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 250, 0)",
@@ -7,8 +8,21 @@ var colors = [
   "rgb(14, 250, 250)",
 ];
 
+//variable declarations
 var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
 var correctColor = colors[3];
+
+colorDisplay.textContent = correctColor;
+
 for(var i = 0; i < squares.length; i++){
-  squares[i].style.backgroundColor = colors[i];
+  squares[i].style.backgroundColor = colors[i]; //setting colors to squares
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.background; //grabbing the color of the selected square
+    if(clickedColor === correctColor){ //comparing with answer
+      alert("CORRECT");
+    }else{
+      this.style.background = "#232323";
+    }
+  });
 }
