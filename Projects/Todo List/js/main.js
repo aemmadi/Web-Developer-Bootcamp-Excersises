a8e2571aea86b50650cdb9d0f9d015fdd5f5d108
Anirudh Emmadi <kanna6501@gmail.com>
2018-05-22 09:23:53 -0500

    Start new Todo-list Project

---
+++ Projects/Todo List/js/main.js
@@ -0,0 +1,9 @@
+$('li').click(function (){
+  if($(this).css("color") == "rgb(128, 128, 128)"){
+    $(this).css("color", "black");
+    $(this).css("text-decoration", "none");
+  }else {
+    $(this).css("color", "gray");
+    $(this).css("text-decoration", "line-through");
+  }
+})
