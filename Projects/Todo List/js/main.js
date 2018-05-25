2999574a77c27bf59155988fa9e114eefdde50f3
Anirudh Emmadi <kanna6501@gmail.com>
2018-05-25 14:06:03 -0500

    Finished lesson 197 added classes

--- Projects/Todo List/js/main.js
+++ Projects/Todo List/js/main.js
@@ -1,12 +1,20 @@
 //Clicking on an item adds/removes the strikethrough effect
-$('li').click(function (){
+$('.content').on("click", "li", function (){
   $(this).toggleClass("done");
 })
 
 //Clicking on the X fades an item out
-$('span').click(function (event){
+$('.content').on("click", ".del", function (event){
   $(this).parent().fadeOut(800, function (){
     $(this.remove());
   });
-  event.stopPropogation();
+  event.stopPropogation; //Stops the events from affecting the whole parent element (ripple effect)
+})
+
+//Hitting enter adds a new Todo item
+$('.textfield').on("keypress", function (event){
+  if(event.which == 13){
+    var input = $(this).val();
+    $('ul').append('<li><span class="del">X</span> ' + input + '</li>');
+  }
 })
